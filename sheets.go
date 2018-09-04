2ed983dffcd1f626ba84d4c7ce4b87299711619b
Tomotaka Sakuma <ktmtmks@gmail.com>
2018-09-05 08:38:33 +0900

    Add Fetch

--- sheets.go
+++ sheets.go
@@ -27,3 +27,27 @@ func NewSheetService(path string) (*sheets.Service, error) {
 
 	return srv, nil
 }
+
+func Fetch(srv *sheets.Service, id, address string) ([][]*sheets.CellData, error) {
+	res, err := srv.Spreadsheets.Get(id).Ranges(address).Fields("sheets(properties,data.rowData.values(formattedValue,note))").Do()
+	if err != nil {
+		return nil, err
+	}
+	return ToCellData2D(res), nil
+}
+
+func ToCellData2D(res *sheets.Spreadsheet) [][]*sheets.CellData {
+	rows := make([][]*sheets.CellData, 0)
+	for _, sheet := range res.Sheets {
+		for _, d := range sheet.Data {
+			for _, e := range d.RowData {
+				l := make([]*sheets.CellData, len(e.Values))
+				for i, v := range e.Values {
+					l[i] = v
+				}
+				rows = append(rows, l)
+			}
+		}
+	}
+	return rows
+}
